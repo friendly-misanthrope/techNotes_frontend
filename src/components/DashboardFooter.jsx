6f2d4c563665648b60d2892f2243d4e61bbde8e0
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-23 04:45:31 -0700

    Added goHome button to dashboard footer that only renders if user isn't already at their dashboard home page

--- src/components/DashboardFooter.jsx
+++ src/components/DashboardFooter.jsx
@@ -3,8 +3,29 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const DashboardFooter = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const onGoHomeClicked = () => {
+    navigate('/dash');
+  }
+
+  let goHomeButton = null;
+
+  if (pathname !== '/dash') {
+    goHomeButton = (
+      <button 
+        className="dash-footer__button icon_button" 
+        title="Home"
+        onClick={ onGoHomeClicked } 
+        >
+        <fontAwesomeIcon icon={ faHouse } />
+      </button>
+    );
+  }
   const content = (
     <footer className="dash-footer">
+      { goHomeButton }
       <p>Current User:</p>
       <p>Status:</p>
     </footer>
