25bd142acf87200a5fecc468a9f722b76dc433b1
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-23 03:58:48 -0700

    Created basic Login component and assigned it '/login' route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import CSS and components here
 import { Routes, Route } from 'react-router-dom';
 import Public from './components/Public';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={ <Public /> } />
+        <Route path="/login" element={ <Login /> } />
 
       </Route>
     </Routes>
