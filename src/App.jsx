7f5ee254e18676c631a437406794490ae3a6028f
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-23 03:55:53 -0700

    Added public-facing page with contact info and login link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import CSS and components here
 import { Routes, Route } from 'react-router-dom';
+import Public from './components/Public';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        
+        <Route index element={ <Public /> } />
+
       </Route>
     </Routes>
   );
--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -1,9 +1,39 @@
 import { Link } from "react-router-dom";
 
 const Public = () => {
-  <section className="public">
-    <header>
-      <h1>Welcome to <span className="nowrap">Welcome to Leo's Repair Shop!</span></h1>
-    </header>
-  </section>
-}
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Welcome to{" "}
+          <span className="nowrap">Welcome to Leo's Repair Shop!</span>
+        </h1>
+      </header>
+
+      <main className="public__main">
+        <p>
+          Located in Beautiful Seattle WA, Leo's Repair Shop provides a trained
+          staff ready to meet your device repair needs.
+        </p>
+        <address className="public__addr">
+          Leo's Repair Shop
+          <br />
+          400 Broad St.
+          <br />
+          Seattle, WA 98109
+          <br />
+          <a href="tel: +12065552100">(206) 555-2100</a>
+        </address>
+        <br />
+        <p>Owner: Leo Nidas</p>
+      </main>
+
+      <footer>
+        <Link to="/login">Employee Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+
+export default Public;
