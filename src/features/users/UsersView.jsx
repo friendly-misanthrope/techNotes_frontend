a01edc7cf86917b61d3d624f4f1821ed2f1e271f
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-03 05:02:04 -0700

    Destructured data from useGetUsersQuery, created content for query's isLoading and isError props

--- src/features/users/UsersView.jsx
+++ src/features/users/UsersView.jsx
@@ -1,4 +1,36 @@
+import { useGetUsersQuery } from "./usersSlice";
+
 const UsersView = () => {
+
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUsersQuery();
+
+  let content;
+
+  // todo: style '.loader'
+  if (isLoading) {
+    content =
+    <>
+      <p className="loader">Loading...</p>
+      <div className="loader">
+        <BallTriangle 
+        height={100}
+        color="#61dbfb" />
+      </div>
+    </>
+  }
+  if (isError) {
+    content =
+    <p className='errmsg'>
+      {error.data.message}
+    </p>
+  }
+
   return (
     <h1>UsersList</h1>
   );
