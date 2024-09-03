2cb58441cc1837ee93c05695467abc92ef6d235c
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-02 18:50:37 -0700

    formatted Welcome component

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -2,11 +2,10 @@ import { Link } from "react-router-dom";
 
 const Welcome = () => {
   const date = new Date();
-  const today = new Intl.DateTimeFormat(
-    'en-US', {
-      dateStyle: 'full',
-      timeStyle: 'long'
-    }).format(date);
+  const today = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeStyle: "long",
+  }).format(date);
 
   const content = (
     <section className="welcome">
@@ -14,11 +13,15 @@ const Welcome = () => {
 
       <h1>Welcome!</h1>
 
-      <p><Link to="/dashboard/notes">View Tech Notes</Link></p>
-      <p><Link to="/dashboard/users">User Settings</Link></p>
+      <p>
+        <Link to="/dashboard/notes">View Tech Notes</Link>
+      </p>
+      <p>
+        <Link to="/dashboard/users">User Settings</Link>
+      </p>
     </section>
   );
   return content;
-}
+};
 
 export default Welcome;
