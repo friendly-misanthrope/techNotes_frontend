72e5573f152ee5618825859628f6004776bc736f
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-03 20:21:19 -0700

    created content for useGetUsersQuery isSuccess

--- src/features/users/UsersView.jsx
+++ src/features/users/UsersView.jsx
@@ -1,39 +1,57 @@
 import { useGetUsersQuery } from "./usersSlice";
 
 const UsersView = () => {
-
   const {
     data: users,
     isLoading,
     isSuccess,
     isError,
-    error
+    error,
   } = useGetUsersQuery();
 
   let content;
 
   // todo: style '.loader'
   if (isLoading) {
-    content =
-    <>
-      <p className="loader">Loading...</p>
-      <div className="loader">
-        <BallTriangle 
-        height={100}
-        color="#61dbfb" />
-      </div>
-    </>
-  }
-  if (isError) {
-    content =
-    <p className='errmsg'>
-      {error.data.message}
-    </p>
+    content = (
+      <>
+        <p className="loader">Loading...</p>
+        <div className="loader">
+          <BallTriangle height={100} color="#61dbfb" />
+        </div>
+      </>
+    );
+  } else if (isError) {
+    content = <p className="errmsg">{error.data.message}</p>;
+  } else if (isSuccess) {
+    const { ids } = users;
+    const usersTable = ids?.length
+      ? ids.map((userId) => {
+          // todo: put User component here
+        })
+      : null;
+
+    content = (
+      <table className="table table--users">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table__th user__username">
+              Username
+            </th>
+            <th scope="col" className="table__th user__roles">
+              Roles
+            </th>
+            <th scope="col" className="table__th user__edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{usersTable}</tbody>
+      </table>
+    );
   }
 
-  return (
-    <h1>UsersList</h1>
-  );
-}
+  return content;
+};
 
 export default UsersView;
