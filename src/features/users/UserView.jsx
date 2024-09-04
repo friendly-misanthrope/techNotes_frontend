b8ef1cca00b78861823f10e4515e3d32d991d5a3
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-03 20:46:17 -0700

    created UserView component for rendering each user as a table row in UsersView

---
+++ src/features/users/UserView.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserById } from './usersSlice';
+
+const UserView = ({ userId }) => {
+  // Hooks
+  const user = useSelector((state) => selectUserById(state, userId));
+  const navigate = useNavigate();
+
+  if (user) {
+    const handleUserEdit = () => navigate(`/dashboard/users/${userId}`);
+    const userRoles = user.roles.toString().replaceAll(',', ', ');
+    const isActive = user.isActive ? '' : 'table__cell--inactive';
+
+  return (
+    <tr className="table__row user">
+      <td className={`table__cell ${isActive}`}>{user.username}</td>
+      <td className={`table__cell ${isActive}`}>{userRoles}</td>
+      <td className={`table_cell ${isActive}`}>
+        <button 
+        className="icon-button table__button"
+        onClick={handleUserEdit}
+        >
+          <FontAwesomeIcon icon={faPenToSquare} />
+        </button>
+      </td>
+    </tr>
+    );
+  }
+  else return null;
+}
+export default UserView;
