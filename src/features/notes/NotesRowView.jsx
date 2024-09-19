be234d974edd30723f9bf77b88a076562a354ec8
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-18 20:18:48 -0700

    Added NotesRowView for rendering each row in Notes table

---
+++ src/features/notes/NotesRowView.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectNoteById } from "./notesSlice";
+
+const NotesRowView = () => {
+  return (
+    <div>NotesRowView</div>
+  )
+}
+export default NotesRowView
