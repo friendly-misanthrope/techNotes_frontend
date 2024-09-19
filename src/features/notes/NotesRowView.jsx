6346c5f651c181920f2aaaefab9d974bcde0f0ff
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-18 21:09:11 -0700

    Created table row content for each Note

--- src/features/notes/NotesRowView.jsx
+++ src/features/notes/NotesRowView.jsx
@@ -4,9 +4,38 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectNoteById } from "./notesSlice";
 
-const NotesRowView = () => {
-  return (
-    <div>NotesRowView</div>
-  )
+const NotesRowView = ({noteId}) => {
+
+  const note = useSelector((state) => selectNoteById(state, noteId));
+  const navigate = useNavigate();
+
+  if (note) {
+    const createdAt = new Date(note.createdAt).toLocaleString('en-US', {day: "numeric", month: "long"});
+    const updatedAt = new Date(note.updatedAt).toLocaleString('en-US', {day: "numeric", month: "long"});
+    const handleEdit = () => navigate(`/dashboard/notes/${note._id}`);
+
+    return (
+      <tr className="table__row">
+        <td className="table__cell note__status">
+          {
+            note.isComplete ?
+              <span className="note__status--completed">Closed</span>
+              : <span className="note__status--open">Open</span>
+          }
+        </td>
+        <td className="table__cell note__created">{createdAt}</td>
+        <td className="table__cell note__updated">{updatedAt}</td>
+        <td className="table__cell note__title">{note.title}</td>
+        <td className="table__cell note__username">{note.assignedUser}</td>
+
+        <td className="table__cell">
+          <button className="icon-button table__button"
+          onClick={handleEdit}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </button>
+        </td>
+      </tr>
+    )
+  } else return null;
 }
-export default NotesRowView
+export default NotesRowView;
