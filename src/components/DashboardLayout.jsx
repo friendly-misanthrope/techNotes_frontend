64ae123ab2caede26ca583e0ba37a5937b2c5396
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-20 02:09:52 -0700

    dispatched actions to retrieve notes and users upon dashboard loading

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,6 +1,12 @@
 import { Outlet } from "react-router-dom";
 import DashboardHeaderView from "./DashboardHeaderView";
 import DashboardFooterView from "./DashboardFooterView";
+import { notesSlice } from "../features/notes/notesSlice";
+import { usersSlice } from "../features/users/usersSlice";
+import { store } from "../app/store";
+
+store.dispatch(notesSlice.endpoints.getNotes).initiate();
+store.dispatch(usersSlice.endpoints.getUsers).initiate();
 
 const DashboardLayout = () => {
   return (
