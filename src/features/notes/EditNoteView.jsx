58b5c187218edb5fefb8e293b846b1e09a561553
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-25 16:49:06 -0700

    Declared hooks for updating an existing post

--- src/features/notes/EditNoteView.jsx
+++ src/features/notes/EditNoteView.jsx
@@ -1,11 +1,23 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
-import { selectNoteById } from "./notesSlice";
+import { selectNoteById, useUpdateNoteMutation } from "./notesSlice";
 
 const EditNoteView = () => {
+  const { noteId } = useParams();
+  const navigate = useNavigate();
+  const noteToEdit = useSelector((state) => selectNoteById(state, noteId));
+  const [updateNote, { isLoading }] = useUpdateNoteMutation();
+  const [editedNote, setEditedNote] = useState({});
+
+  useEffect(() => {
+    if (noteToEdit) {
+      setEditedNote(noteToEdit);
+    }
+  }, [noteToEdit]);
+
   return (
     <div>EditNoteView</div>
   )
 }
-export default EditNoteView
+export default EditNoteView;
