4c1520706a0de1bb9f7d430b3199922a64dd9616
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-25 15:26:40 -0700

    created EditNoteView form component

---
+++ src/features/notes/EditNoteView.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { selectNoteById } from "./notesSlice";
+
+const EditNoteView = () => {
+  return (
+    <div>EditNoteView</div>
+  )
+}
+export default EditNoteView
