632b23b830b6af2f83f3416018933cce19b70ec0
friendly-misanthrope <nickthedevguy@gmail.com>
2024-09-25 17:30:42 -0700

    created user select options for EditNoteView form

--- src/features/notes/EditNoteView.jsx
+++ src/features/notes/EditNoteView.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
 import { selectNoteById, useUpdateNoteMutation } from "./notesSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const EditNoteView = () => {
   const { noteId } = useParams();
@@ -16,8 +17,43 @@ const EditNoteView = () => {
     }
   }, [noteToEdit]);
 
+  const {
+    assignedUser,
+    title,
+    content,
+    isCompleted
+  } = editedNote;
+
+  assignedUser = assignedUser.username;
+
+  const allUsers = useSelector((state) => selectAllUsers(state));
+
+  const employeeOptions = allUsers.map((user) => (
+    <option 
+    key={user._id}
+    value={user._id}>
+      {user.username}
+    </option>
+  ));
+
   return (
-    <div>EditNoteView</div>
-  )
-}
+    <section className="note-edit">
+      <form>
+        
+        <div className="form-group">
+          <label for="username">Employee:</label>
+          <select
+            type="text"
+            className="form-control"
+            name="assignedUser"
+            value={assignedUser}
+          >
+            {employeeOptions}
+          </select>
+        </div>
+        
+      </form>
+    </section>
+  );
+};
 export default EditNoteView;
