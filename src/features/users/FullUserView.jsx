a5f1e9d4371478c44899af9a24b9e5022dc40ff4
friendly-misanthrope <nickthedevguy@gmail.com>
2024-10-06 04:25:02 -0700

    declared hooks/component state, added formatted createdAt/updatedAt dates

--- src/features/users/FullUserView.jsx
+++ src/features/users/FullUserView.jsx
@@ -1,6 +1,38 @@
+import { useParams, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserById } from './usersSlice';
+import { useState, useEffect } from 'react';
+import oops from '../../public/img/oops.jpg';
+
 const FullUserView = () => {
+  const { userId } = useParams();
+  const user = useSelector((state) => selectUserById(state, userId));
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    if (user) {
+      setUserData(user);
+    }
+  },[user]);
+
+  const {
+    username,
+    roles,
+    isActive,
+    notes,
+    createdAt,
+    updatedAt
+  } = userData;
+
+  const created = new Date(createdAt).toLocaleString('en-US', {day: "numeric", month: "long", year: "numeric"});
+  const updated = new Date(updatedAt).toLocaleString('en-US', {day: "numeric", month: "long", year: "numeric"});
+
+
+
   return (
-    <div>FullUserView</div>
-  )
+    <div>
+      Full User View
+    </div>
+  );
 }
 export default FullUserView;
