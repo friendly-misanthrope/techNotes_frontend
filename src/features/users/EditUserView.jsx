2abbd4489281deab75b3c8e2f9e1e81a188063f1
friendly-misanthrope <nickthedevguy@gmail.com>
2024-10-15 03:06:52 -0700

    added title, form and username input for editing user

--- src/features/users/EditUserView.jsx
+++ src/features/users/EditUserView.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { selectUserById, useUpdateUserMutation } from "./usersSlice";
+
 const EditUserView = () => {
+
+
   return (
-    <div>EditUserView</div>
+    <div className="data-container">
+      <h2 className="data-title">Edit [ username goes here ]</h2>
+      <section className="full-card">
+        <article className="full-data__card">
+
+          <form>
+
+            <div className="form-group full-card__section">
+              <label htmlFor="username"
+              className="card-section__header">Username</label>
+              <input type="text"
+              className="form-control"
+              name="username" />
+            </div>
+            
+          </form>
+        </article>
+      </section>
+    </div>
   )
 }
 export default EditUserView;
