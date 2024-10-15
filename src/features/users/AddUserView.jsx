adeb70418e63582d42c7b939b18da3c329bbb515
friendly-misanthrope <nickthedevguy@gmail.com>
2024-10-15 03:17:30 -0700

    Added user REGEX expressions

--- src/features/users/AddUserView.jsx
+++ src/features/users/AddUserView.jsx
@@ -5,6 +5,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
 
+const VALID_USER = /^[A-z]{4,24}$/
+const VALID_PWD = /^[A-z0-9!@#$%]{8,32}$/
+
 const AddUserView = () => {
   return (
     <div>AddUserView</div>
